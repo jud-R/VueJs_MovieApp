7d2b546aa46628e8bf81ad4dedaadc0f1ac13a91
Jud.RODRIGUES <r.judicael@gmail.com>
2020-01-27 10:36:42 +0100

    first Jest test for Search component

---
+++ src/stack.js
@@ -0,0 +1,14 @@
+class Stack {
+    constructor(capacity) {
+        this.items = []
+        this.capacity = capacity
+    }
+    isEmpty () {
+      return this.items.length === 0
+    }
+    isFull () {
+        return this.items.length === this.capacity
+      }
+  }
+   
+  export default Stack
