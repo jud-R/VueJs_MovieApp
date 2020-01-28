1fe6b90fc2f4528aca3d22b8a1e3cdafe7618779
Jud.RODRIGUES <r.judicael@gmail.com>
2020-01-28 16:37:54 +0100

    add default image when no image to display

--- src/components/MovieListItem.vue
+++ src/components/MovieListItem.vue
@@ -1,6 +1,6 @@
 <template>
       <li>
-        <img :src="movie.Poster"/>
+        <img :src="imageURL"/>
         {{movie.Year}}
         {{movie.Title}}
         <a :href="`https://www.imdb.com/title/${movie.imdbID}`" target="_blank">More</a>
@@ -8,6 +8,8 @@
 </template>
 
 <script>
+import unknownImage from '@/assets/unknown.jpg'
+
 export default {
   name: 'movie-list-item',
   props: {
@@ -15,7 +17,12 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+  imageURL () {
+    return this.movie.Poster ==="N/A" ? unknownImage : this.movie.Poster
   }
+}
 }
 </script>
 
